2a751516f76e6861a33dc5a0bbb8100b514b6b5c
Davi Neves <davivirgula@gmail.com>
2023-04-23 15:03:43 -0300

    Update 'Relatorio' for 'Trabalho 1' and configurate the workspace for 'Trabalho 2'.

---
+++ Trabalho 2 - Redes Neurais/perceptron.py
@@ -0,0 +1,62 @@
+"""
+Trabalho 2 - Redes Neurais
+Data de Entrega: 11/05/2023
+
+Aluno: Davi Augusto Neves Leite
+RA: CCO230111
+"""
+
+import numpy as np               # Matrizes e Funções Matemáticas
+
+#####################################################
+#              Funções de Avaliação                 #
+#####################################################
+
+# Exercício 01: Iris Data Set (https://archive.ics.uci.edu/ml/datasets/Iris)
+
+
+# Exercício 02: Wine Data Set (https://archive.ics.uci.edu/ml/datasets/Wine)
+
+
+# Exercício 03: Video Games Rating By 'ESRB' (https://www.kaggle.com/datasets/imohtn/video-games-rating-by-esrb)
+
+
+
+#####################################################
+#             Rede Neural: Perceptron               #
+#####################################################
+
+
+########################################
+#            Experimentos              #
+########################################
+
+
+def main():
+    """Função principal do programa
+    
+    Descrição dos Experimentos:
+        
+        
+    Restrições:
+        
+    
+    Formato dos dados salvos:
+        - Gráfico (por ciclo):
+        - Tabelas (por ciclo):
+    """
+
+    #! [Debug] Definição de saída para mostrar as matrizes por completo no console se necessário.
+    np.set_printoptions(threshold=np.inf)
+
+    # Biblioteca para capturar o tempo de execução
+    from time import time
+
+    
+
+if __name__ == '__main__':
+    """Ponto de entrada do programa
+    """
+
+    # Chama a função principal
+    main()
