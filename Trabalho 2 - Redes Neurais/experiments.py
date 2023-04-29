a6d2c2b30eb2adb96d54f40fa49a1bcd21107152
Davi Neves <davivirgula@gmail.com>
2023-04-28 21:29:35 -0300

    like take a candy from a baby? i don't know... these experiments are going to blow up my mind in the next days...

---
+++ Trabalho 2 - Redes Neurais/experiments.py
@@ -0,0 +1,198 @@
+"""
+Trabalho 2 - Redes Neurais
+Data de Entrega: 11/05/2023
+
+Aluno: Davi Augusto Neves Leite
+RA: CCO230111
+"""
+
+import numpy as np # Matrizes e Funções Matemáticas
+import sklearn.datasets as sk_data # Recuperar os Datasets
+from sklearn.preprocessing import StandardScaler  # Normalização dos Dados de Pré-Processamento com Z Score
+from sklearn.model_selection import train_test_split # Separar conjuntos de treinamento, testes e validação
+from sklearn.metrics import accuracy_score # Métrica de Acurácia para Experimentos
+import matplotlib.pyplot as plt # Criação de Gráficos
+#import pandas as pd # Manipulação e Visualização dos Datasets
+
+from perceptron import Perceptron # Rede Neural: Perceptron
+
+#####################################################
+#                  Base de Dados                    #
+#####################################################
+
+# Exercício 01: Iris Dataset (https://archive.ics.uci.edu/ml/datasets/Iris)
+def load_iris_dataset() -> tuple[np.ndarray, np.ndarray]:
+    """Carrega o "Iris" dataset, a partir
+    da biblioteca "sklearn". 
+    
+    - Informações do dataset em: https://archive.ics.uci.edu/ml/datasets/Iris
+    
+    Returns:
+        (data, target) : (np.ndarray, np.ndarray)
+            tupla de ndarray do tipo:
+            data: matriz 2D de forma (num_amostras, num_características) com cada linha representando uma amostra e cada coluna representando as características.
+            target: matriz 1D de forma (num_amostras) contendo as amostras de destino.
+         
+    """
+
+    return sk_data.load_iris(return_X_y=True)
+
+
+# Exercício 02: Wine Data Set (https://archive.ics.uci.edu/ml/datasets/Wine)
+def load_wine_dataset() -> tuple[np.ndarray, np.ndarray]:
+    """Carrega o "Wine" dataset, a partir
+    da biblioteca "sklearn". 
+    
+    - Informações do dataset em: https://archive.ics.uci.edu/ml/datasets/Wine
+    
+    Returns:
+        (data, target) : (np.ndarray, np.ndarray)
+            tupla de ndarray do tipo:
+            data: matriz 2D de forma (num_amostras, num_características) com cada linha representando uma amostra e cada coluna representando as características.
+            target: matriz 1D de forma (num_amostras) contendo as amostras de destino.
+         
+    """
+
+    return sk_data.load_wine(return_X_y=True)
+
+
+# Exercício 03:
+
+
+########################################
+#            Experimentos              #
+########################################
+
+# Treinamento do Perceptron (Experimento)
+def perceptron_experiment(
+    max_exp_per_cycle: int,
+    X: np.ndarray, 
+    y: np.ndarray,
+    initial_W: np.ndarray,
+    initial_bias: float,
+    initial_learning_rate: float,
+):
+    """ Realização dos experimentos com Perceptron
+
+    Args:
+        max_exp_per_cycle : int 
+            Número máximo de experimentos
+        X : np.ndarray
+            matriz 2D de forma (num_amostras, num_características) com cada linha representando uma amostra e cada coluna representando as características.
+        y : np.ndarray
+            matriz 1D de forma (num_amostras) contendo as amostras de destino.
+        initial_W : np.ndarray
+            Vetor de pesos iniciais do Perceptron, que é atualizado durante a fase de treinamento (fit).
+        initial_bias : float
+            Valor inicial de bias do Perceptron, que é atualizado durante a fase de treinamento (fit).
+        initial_learning_rate : float
+            Taxa inicial de aprendizado para atualização dos pesos.
+            
+    Notes:
+        
+    """
+    return None
+
+
+# Manipular arquivos CSV (tabelas)
+def create_csv_table(
+    filename: str,
+    rows: np.ndarray
+):
+    """Escreve os experimentos em um arquivo CSV para tabelas do relatório.
+
+    Args:
+        filename : str 
+            Nome do arquivo/exercicio (ex: 'ex01')
+        rows : np.ndarray[[dados, ...], ...] 
+            Lista com os dados das linhas no total (ex: [['10', '0.1'], ['20', '0.2'])
+        
+    Notes:
+        Cria um arquivo csv da tabela com o seguinte nome: {}.csv  
+        Salva em um subdiretório da pasta local do código com o nome: {filename}
+    """
+
+    # Defininido o sub-diretório dos dados
+    import os
+    actual_dir = os.path.dirname(__file__)
+    sub_directory = os.path.join(actual_dir, f'{filename}/')
+    os.makedirs(sub_directory, exist_ok=True)
+
+    # Definindo o nome do arquivo
+    table_name = f'.csv'
+
+    # Definindo o título da tabela com base no tipo de experimento
+    table_header = 'Title,'
+
+    # Escrevendo o arquivo com o título
+    np.savetxt(fname=os.path.join(sub_directory, table_name), X=rows, header=table_header,
+               delimiter=',', comments='', encoding='UTF-8')  # , fmt='%.4f')
+
+
+def main():
+    """Função principal do programa
+    
+    Restrições:
+        - Para cada ciclo, obtém os melhores pesos e taxas (bias e aprendizado)
+        - Após a execução total do ciclo, executa-se mais uma vez com os melhores parâmetros obtidos para salvar os dados
+        - Repete o processo para a quantidade total de ciclos
+        
+    Descrição dos Experimentos:
+        - 3 ciclos executados
+        - cada ciclo: 15+1 vezes de execução do algoritmo
+        - conjunto de dados iniciais normalizado com z score
+        - valores aletórios para os subconjuntos de treinamento, validação e testes
+        - num. máx. iterações/épocas: 1000
+        - valores iniciais de W (pesos): 0
+        - taxa inicial de "bias": 0
+        - taxa inicial de "Learning Rate": 0.1
+        
+    
+    Formato dos dados salvos:
+        - Gráfico (por ciclo): erro médio quadrático da convergência com a última rede do ciclo
+        - Tabelas (por exercício): 
+            - matrizes de confusão: treinamento, validação e teste
+            - parâmetros iniciais e finais das redes: *pesos, *bias, taxa de aprendizado, taxas de acerto e erro
+    """
+
+    #! [Debug] Definição de saída para mostrar as matrizes por completo no console se necessário.
+    np.set_printoptions(threshold=np.inf)
+
+    # Biblioteca para capturar o tempo de execução
+    from time import time
+
+    ########################################
+    #!     Exercício 01: Iris Dataset     !#
+    ########################################
+
+    # Definindo os atributos iniciais dos experimentos
+    filename = 'ex01'
+    max_cycle = 3
+    max_exp_per_cycle = 15
+    X, y = load_iris_dataset()
+    W_initial = np.zeros(X.shape[1])
+    bias_initial = 0.0
+    learning_rate = 0.1
+    max_it = 100
+
+
+
+    ########################################
+    #!     Exercício 02: Wine Dataset     !#
+    ########################################
+
+
+
+    ########################################
+    #!            Exercício 03            !#
+    ########################################
+
+
+
+
+if __name__ == '__main__':
+    """Ponto de entrada do programa
+    """
+
+    # Chama a função principal
+    main()
--- Trabalho 2 - Redes Neurais/perceptron.py
+++ Trabalho 2 - Redes Neurais/perceptron.py
@@ -7,62 +7,101 @@ RA: CCO230111
 """
 
 import numpy as np               # Matrizes e Funções Matemáticas
-import sklearn as sk             # Recuperar os Datasets
-
-#####################################################
-#              Funções de Avaliação                 #
-#####################################################
-
-# Exercício 01: Iris Data Set (https://archive.ics.uci.edu/ml/datasets/Iris)
-
-
-# Exercício 02: Wine Data Set (https://archive.ics.uci.edu/ml/datasets/Wine)
-
-
-# Exercício 03: Video Games Rating By 'ESRB' (https://www.kaggle.com/datasets/imohtn/video-games-rating-by-esrb)
-
 
 
 #####################################################
 #             Rede Neural: Perceptron               #
 #####################################################
+class Perceptron:
+    """Perceptron Simples para Classificação de Padrões.
 
-
-#####################################################
-#           RN: Perceptron Multi-Camadas            #
-#####################################################
-
-
-########################################
-#            Experimentos              #
-########################################
-
-
-def main():
-    """Função principal do programa
+    Args:
+        W : np.ndarray (tam: X.shape[1] ou num_caracteristicas)
+            Vetor de pesos do Perceptron, que é atualizado durante a fase de treinamento (fit).
+        bias : float
+            Valor de bias do Perceptron, que é atualizado durante a fase de treinamento (fit).
+        learning_rate : float
+            Taxa de aprendizado para atualização dos pesos.
+        max_it : int
+            Número máximo de iterações como critério de parada.
     
-    Descrição dos Experimentos:
+    Attributes:
+        W : np.ndarray
+            Vetor de pesos do Perceptron, que é atualizado durante a fase de treinamento (fit).
+        bias : float
+            Valor de bias do Perceptron, que é atualizado durante a fase de treinamento (fit).
+        learning_rate : float
+            Taxa de aprendizado para atualização dos pesos.
+        max_it : int
+            Número máximo de iterações como critério de parada.
+        activation_function : function
+            Função de ativação utilizada pelo Perceptron. Padrão: função degrau unitário (step function).
+        mse : np.ndarray
+            Vetor que armazena os valores do erro médio quadrático (MSE) durante a fase de treinamento. 
+            Cada elemento do vetor corresponde ao MSE calculado após uma iteração (ou época) do algoritmo.
+            Para o Perceptron, quanto menor o valor do MSE, melhor é o ajuste da rede aos dados de treinamento.
+            
+    Methods:
+        fit (X: np.ndarray, d: np.ndarray)
+            Realiza o treinamento do Perceptron, com base nos parâmetros de inicialização.
+                X: matriz com os valores de entrada
+                d: vetor de saídas desejadas (para erro)
         
-        
-    Restrições:
-        
-    
-    Formato dos dados salvos:
-        - Gráfico (por ciclo):
-        - Tabelas (por ciclo):
+        predict (X: np.ndarray) 
+            Recebe um ndarray e calcula o Perceptron equivalente, após a fase de treinamento 
+                X: matriz com os valores de entrada
     """
 
-    #! [Debug] Definição de saída para mostrar as matrizes por completo no console se necessário.
-    np.set_printoptions(threshold=np.inf)
+    def __init__(self, W, bias, learning_rate, max_it) -> None:
+        self.W = W
+        self.bias = bias
+        self.learning_rate = learning_rate
+        self.max_it = max_it
+        self.activation_function = self.__unit_step_func__
+        self.mse = None
 
-    # Biblioteca para capturar o tempo de execução
-    from time import time
+    def __unit_step_func__(self, x):
+        return np.where(x >= 0, 1, -1)
 
-    
+    def fit(self, X: np.ndarray, d: np.ndarray):
+        # Obtendo a quantidade de amostras e características
+        n_samples, n_features = X.shape
 
-if __name__ == '__main__':
-    """Ponto de entrada do programa
-    """
+        #! Inicializar W (pesos) e bias separadamente
 
-    # Chama a função principal
-    main()
+        # Percorrendo as épocas/iterações
+        t = 1
+        E = 1
+        self.mse = []
+        while t < self.max_it and E > 0:
+            E = 0
+
+            # Percorrendo o padrão de treinamento para a época/iteração 't'
+            for i in range(n_samples):
+                # Obtendo a saída da rede para X[i]
+                y = self.activation_function(np.dot(X[i], self.W) + self.bias)
+
+                # Determinando o erro para X[i]
+                error = d[i] - y
+
+                # Atualizando vetor de pesos
+                self.W += self.learning_rate * error * X[i]
+
+                # Atualizando o bias
+                self.bias += self.learning_rate * error
+
+                # Acumulando o erro quadrático
+                E += error ** 2
+
+            # Salvando o erro médio quadrático de uma época
+            self.mse.append(E / n_samples)
+
+            # Incrementando a iteração
+            t += 1
+
+        # Convertendo os erros salvos para NumPy
+        self.mse = np.array(self.mse, dtype=np.float64)
+
+    def predict(self, X: np.ndarray) -> np.ndarray:
+        # Para predizer, basta aplicar os dados à função de ativação e o último bias obtido
+        return self.activation_function(np.dot(X, self.W) + self.bias)
